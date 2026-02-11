d23da4027dad6212ae9cadb073f346ec24b51b29
BichoyEmad <bichoy.emad@gmail.com>
2026-02-11 18:40:59 +0200

    revamp: splash, login & register screens

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color brand = Color.fromARGB(255, 246, 233, 175);
-  static const Color brandSecondary = Color.fromARGB(89, 246, 233, 175);
-  static const Color textFieldHint = Color.fromARGB(153, 255, 255, 255);
-  static const Color textFieldBackground = Color.fromARGB(147, 19, 19, 19);
+  static const Color brand = Color.fromARGB(255, 139, 69, 19);
+  static const Color brandSecondary = Color.fromARGB(89, 139, 69, 19);
+  static const Color textFieldHint = Color.fromARGB(215, 255, 255, 255);
+  static const Color textFieldBackground = Color.fromARGB(176, 19, 19, 19);
 }
